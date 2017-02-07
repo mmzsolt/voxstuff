f58cf5eca930531af37a79c8acfacc93a24fa6d8
Zsolt Minier <mmzsolt@yahoo.com>
2017-02-07 22:10:05 +0200

    integrated sdl

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # voxstuff
+
+silly little voxely stuff
+
+# building
+
+download SDL2-devel-2.0.5-VC.zip and extract the lib and include directories found in it into extern/sdl
+make a build directory and cd into it
+cmake -G "Visual Studio 14 Win64" . ..
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,27 @@
 #include "Eigen/Core"
 #include "vox.hpp"
 #include <iostream>
+#include "SDL.h"
 
-int main()
+SDL_Window *mainWindow;
+
+int SDL_main(int argc, char *argv[])
 {
 	Eigen::Matrix4f mat = Eigen::Matrix4f::Identity();
 	Eigen::Vector4f vec(1.0f, 2.0f, 3.0f, 4.0f);
 	vec = 2 * mat * vec;
 	std::cout << vec;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		std::cout << "Failed to init SDL\n";
+		return false;
+	}
+
+	mainWindow = SDL_CreateWindow("vox", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		512, 512, SDL_WINDOW_OPENGL);
+
+	SDL_Quit();
 
 	return 0;
 }
